f6ac5285b5fb7e26950420fdf20cf159b6cafcb9
rushisawane <rushisawane@gmail.com>
2017-09-24 17:24:40 +0530

    Created new employee component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EmpComponent } from './emp/emp.cpmponent';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/emp.ts
@@ -0,0 +1,13 @@
+export class Emp
+{
+    eno:number;
+    ename:string='';
+    sal:number;
+    constructor(eno,ename,sal)
+    {
+        this.eno=eno;
+        this.ename=ename;
+        this.sal=sal;
+
+    }
+}
